e59c9e6f2bec65dc1d337b2cb36665423decd58e
abdelhady <bodyyousef169@gmail.com>
2024-04-24 12:02:24 +0200

    Add project files.

---
+++ DevCreedPractical1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace DevCreedPractical1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
