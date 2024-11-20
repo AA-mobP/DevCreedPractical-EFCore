96140ed3dc8ca5665527a81931ff880b08cb712c
abdelhady ashraf <bodyyousef169@gmail.com>
2024-11-20 18:09:12 +0200

    4 Save new recorder .Add()

--- DevCreedPractical1/Program.cs
+++ DevCreedPractical1/Program.cs
@@ -1,11 +1,17 @@
 ﻿
+using DevCreedPractical1.Models;
+
 namespace DevCreedPractical1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            AppDbContext context = new();
+            Blog blog = new Blog { Url = "https://wwww.blogs.com" };
             
+            context.tblBlogs.Add(blog);
+            context.SaveChanges();
         }
     }
 }
