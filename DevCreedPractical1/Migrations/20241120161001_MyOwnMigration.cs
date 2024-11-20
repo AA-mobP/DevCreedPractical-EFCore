8b14200b9f0a17a3351ec988d98436426e4f6264
abdelhady ashraf <bodyyousef169@gmail.com>
2024-11-20 18:16:09 +0200

    6 Add My Own Migration

---
+++ DevCreedPractical1/Migrations/20241120161001_MyOwnMigration.cs
@@ -0,0 +1,23 @@
+﻿using DevCreedPractical1.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevCreedPractical1.Migrations
+{
+    /// <inheritdoc />
+    public partial class MyOwnMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO dbo.tblBlogs VALUES('https://wwww.blogs.com/2');");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM dbo.tblBlogs WHERE Url = 'https://wwww.blogs.com/2';");
+        }
+    }
+}
